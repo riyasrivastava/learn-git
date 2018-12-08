3a9993fc28069ecdda0cbdbf42f97161eda0429a
Riya <riya.srivastava@glaucuslogistics.com>
2018-12-08 12:52:35 +0530

    first commit

---
+++ app/src/main/java/app/androidwear/com/androidwearapp/MainActivity.java
@@ -0,0 +1,35 @@
+package app.androidwear.com.androidwearapp;
+
+import android.os.Bundle;
+import android.support.wearable.activity.WearableActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends WearableActivity {
+
+    TextView num1;
+    TextView num2;
+    Button btn;
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        num1 = (TextView) findViewById(R.id.num1);
+        num2 = (TextView)findViewById(R.id.num2);
+        btn = (Button)findViewById(R.id.btn);
+        txt = (TextView)findViewById(R.id.result);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txt.setText(""+Integer.parseInt(num1.getText().toString()) + Integer.parseInt(num2.getText().toString()));
+            }
+        });
+
+        // Enables Always-on
+        setAmbientEnabled();
+    }
+}
